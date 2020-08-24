2a5641a128302adcfc1b5b7ea22334ad6b3a0c4f
Talshem <talsh8797@gmail.com>
2020-08-24 18:30:15 +0300

    tests 1 + 2

--- server/app.js
+++ server/app.js
@@ -9,23 +9,24 @@ var data = JSON.parse(file);
 
 
 app.get('/api/tickets', (req, res) => {
+let filter = [];
+let searchText = req.query;
+if(searchText.searchText){
+for (let ticket of data) {
+    if(ticket.title.toUpperCase().includes(searchText.searchText.toUpperCase())){
+    filter.push(ticket)
+    }
+}
+res.send(filter)
+}
+else{
 res.send(data)
+}
 });
 
-app.get('/api/tickets/:ticketid', (req, res) => {
+app.post('/api/tickets/:ticketid/done', (req, res) => {
     for(let ticket of data) {
         if(ticket.id === req.params.ticketid) {
-            res.send(ticket);
-        }
-    else {
-        res.send('There is no ticket with the corresponding ID');
-    }
-}})
-
-
-app.post('/api/tickets/:id/done', (req, res) => {
-    for(let ticket of data) {
-        if(ticket.id === req.params.id) {
             ticket.done = true
             res.send(ticket);
         }
@@ -35,7 +36,7 @@ app.post('/api/tickets/:id/done', (req, res) => {
 }})
 
 
-app.post('/api/tickets/:id/undone', (req, res) => {
+app.post('/api/tickets/:ticketid/undone', (req, res) => {
     for(let ticket of data) {
         if(ticket.id === req.params.id) {
             ticket.done = false
--- server/index.js
+++ server/index.js
@@ -1,9 +1,4 @@
 const app = require('./app');
 const port = 8080;
 
-app.get('/data', (req, res) => {
-res.send(data)
-});
-
-
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
