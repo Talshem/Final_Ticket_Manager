cbcb4e57a60971540ea5b0869bb608ca8e4eb3b9
Talshem <talsh8797@gmail.com>
2020-08-24 21:24:05 +0300

    labels

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -6,6 +6,7 @@ import { set } from 'object-path';
 function Ticket() {
 const [list, setList] = useState(0)
 
+
   useEffect(() => {
   async function fetchData() {
   const data = await axios.get('/api/tickets');
@@ -15,17 +16,27 @@ const [list, setList] = useState(0)
   fetchData()}, );
 
 const makeTickets = (array) => {
+
 const tickets = array.map((e) => {
 let date = new Date(e.creationTime)
+let x = [];
+
+if(e.labels){
+x = e.labels
+}
 
  return  (
 <div className="ticket" key={e.id}>
 <h3>{e.title}</h3>
 <p>{e.content}</p>
- <p>By {e.userEmail} | {date.toISOString().substr(0, 19).replace('T', ', ')} {Number(date.toISOString().substr(11, 2)) > 11 ? 'PM' : 'AM'}</p>
-<p></p>
+<p>By {e.userEmail} |
+      {date.toISOString().substr(0, 19).replace('T', ', ')}
+      {Number(date.toISOString().substr(11, 2)) > 11 ? 'PM' : 'AM'}
+      {x.map(e => <p className="label">{e}</p>)}
+</p>
 </div>
 );
+
 })
 setList(tickets)
 }
