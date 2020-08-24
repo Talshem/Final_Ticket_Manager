472e45797a798b5126c799c3d53deaaba0cb9034
Talshem <talsh8797@gmail.com>
2020-08-24 20:02:56 +0300

    styling

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,8 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+
 function Ticket() {
-const [id, setId] = useState(0)
+const [list, setList] = useState(0)
 
   useEffect(() => {
   async function fetchData() {
@@ -15,20 +16,19 @@ const [id, setId] = useState(0)
 
 const makeTickets = (array) => {
 const tickets = array.map(e =>
-<div>
-<p>{e.id}</p>
-<p>{e.title}</p>
+<div className="ticket" key={e.id}>
+<h3>{e.title}</h3>
 <p>{e.content}</p>
-<p>{e.userEmail}</p>
-<p>{e.labels}</p>
+<p>By {e.userEmail} | {e.creationTime}</p>
+<p></p>
 </div>
 )
-setId(tickets)
+setList(tickets)
 }
 
   return (
     <main>
-      <p>{id}</p>
+      {list}
     </main>
   );
 }
