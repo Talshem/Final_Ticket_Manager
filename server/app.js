5d550e6aebafe312b3b4a6a3eb7d4a5090cbe3c7
Talshem <talsh8797@gmail.com>
2020-08-24 16:34:13 +0300

    + post done and undone

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ app.get('/api/tickets', (req, res) => {
 res.send(data)
 });
 
-app.get('/api/tickets/:id', (req, res) => {
+app.get('/api/tickets/:ticketid', (req, res) => {
     for(let ticket of data) {
-        if(ticket.id === req.params.id) {
+        if(ticket.id === req.params.ticketid) {
             res.send(ticket);
         }
     else {
@@ -23,26 +23,29 @@ app.get('/api/tickets/:id', (req, res) => {
 }})
 
 
+app.post('/api/tickets/:id/done', (req, res) => {
+    for(let ticket of data) {
+        if(ticket.id === req.params.id) {
+            ticket.done = true
+            res.send(ticket);
+        }
+    else {
+        res.send('There is no ticket with the corresponding ID');
+    }
+}})
 
-app.post('/api/tickets/:id/undone', (req, res) => 
-{   
-    res.send(req.body);
-    records.push(req.body);
-    fs.writeFile('/records.json', JSON.stringify(records), () => {
-    console.log(records)
-    });
-    res.send()
-});
 
-app.post('/api/tickets/:id/done', (req, res) => 
-{   
-    res.send(req.body);
-    records.push(req.body);
-    fs.writeFile('/records.json', JSON.stringify(records), () => {
-    console.log(records)
-    });
-    res.send()
-});
+app.post('/api/tickets/:id/undone', (req, res) => {
+    for(let ticket of data) {
+        if(ticket.id === req.params.id) {
+            ticket.done = false
+            res.send(ticket);
+        }
+    else {
+        res.send('There is no ticket with the corresponding ID');
+    }
+}})
+
 
 app.use(bodyParser.json())
 app.use(express.static('../client/build'))
